44d95360a48337668c519e4e2905c78663c215a0
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-03 13:21:16 -0600

    Creating the environment variables

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { engine } = require('express-handlebars')
 
 const router = require('./routes')
 
+require('dotenv').config()
+
 const app = express()
 
 app.engine('handlebars', engine())
@@ -13,4 +15,4 @@ app.use(express.static('public'))
 
 app.use('/', router())
 
-app.listen(5000)
+app.listen(process.env.PORT)
