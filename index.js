f84d4f6555221b9732849b8254eb690e7e3f9173
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-03 10:47:03 -0600

    Enabling Handlebars with Template Engine

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
+
 const router = require('./routes')
 
 const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
 
 app.use('/', router())
 
