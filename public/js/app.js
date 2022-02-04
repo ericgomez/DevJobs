61515581ffacd245fa3d6273e4f13acd7ac1eb4f
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-04 12:37:23 -0600

    Saving a Set with the skills for the vacancy

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
-console.log('Hello World!')
+// Esperamos hasta que el DOM esté listo
+document.addEventListener('DOMContentLoaded', () => {
+  const skills = document.querySelector('.list-skills')
+
+  if (skills) {
+    skills.addEventListener('click', addSkills)
+  }
+})
+
+const skills = new Set()
+
+const addSkills = e => {
+  if (e.target.tagName === 'LI') {
+    if (e.target.classList.contains('active')) {
+      e.target.classList.remove('active')
+      skills.delete(e.target.textContent)
+    } else {
+      e.target.classList.add('active')
+      skills.add(e.target.textContent)
+    }
+  }
+
+  const skillsArray = [...skills]
+  document.querySelector('#skills').value = skillsArray
+}
